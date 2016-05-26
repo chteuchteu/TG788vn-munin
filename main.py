53d7eaaa8f5d1605f99a8097eae816c62025132c
Quentin Stoeckel <chteuchteu@gmail.com>
2016-05-26 10:56:57 +0200

    Script v1

---
+++ main.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+import re
+import telnetlib
+import argparse
+import sys
+import os
+import pickle
+
+# Credentials
+HOST = ''
+USER = ''
+PSWD = ''
+
+LNRT = '\r\n'
+PRPT = '=>'
+
+IF_LOG_FILE_PATH = '/tmp/TG788vn-munin-traffic.log'
+
+# Mode: either 'tg788vn-bandwidth' or 'tg788vn-traffic', determine by symlink name
+mode = __file__.split('/')[-1]
+modes = ['tg788vn-bandwidth', 'tg788vn-traffic']
+
+if mode not in modes:
+    sys.exit(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', nargs='?')
+args = parser.parse_args()
+
+if args.config == 'config':
+    if mode == 'tg788vn-bandwidth':
+        print('graph_title XDSL bandwidth')
+        print('graph_vlabel bandwidth')
+        print('graph_category router')
+        print('up.label Up')
+        print('down.label Down')
+    elif mode == 'tg788vn-traffic':
+        print('graph_title Internet interface stats')
+        print('graph_vlabel usage')
+        print('graph_category router')
+        print('up.label Up (B/s)')
+        print('up.draw AREA')
+        print('down.label Down (B/s)')
+        print('down.draw STACK')
+
+    sys.exit(0)
+
+# Connect to host & authenticate
+#print('Connecting...')
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b'Username : ', 2)
+tn.write(USER + LNRT)
+#print(' -> {}'.format(USER))
+
+tn.read_until(b'Password : ', 2)
+tn.write(PSWD + LNRT)
+#print(' -> {}'.format('*' * len(PSWD)))
+
+tn.read_until(PRPT)
+
+# Query data
+if mode == 'tg788vn-bandwidth':
+    tn.write('xdsl' + LNRT)
+    tn.read_until(PRPT)
+    tn.write('info' + LNRT)
+    xdsl_stats = tn.read_until(PRPT)
+    tn.write('exit')
+    tn.close()
+
+    # Parse xdsl stats
+
+    # Modem state:                             up
+    # Up time (Days hh:mm:ss):                 0 jours, 1:06:47
+    # xDSL Type:                               ADSL2+
+    # Bandwidth (Down/Up - kbit/s):            20108/1032
+
+    for row in xdsl_stats.split('\n'):
+        if ': ' in row:
+            key, value = row.split(': ')
+            if key == 'Bandwidth (Down/Up - kbit/s)':
+                values = value.lstrip().split('/')
+
+                print('up.value {}'.format(values[1]))
+                print('down.value {}'.format(values[0]))
+
+                sys.exit(0)
+
+elif mode == 'tg788vn-traffic':
+    tn.write('ip' + LNRT)
+    tn.read_until(PRPT)
+    tn.write('iflist' + LNRT)
+    if_stats = tn.read_until(PRPT)
+    tn.write('exit')
+    tn.close()
+
+    # Parse if stats
+    # Interface                            Group  MTU   RX         TX         Admin  Oper
+    # 1   loop. . . . . . . . . . . . . .  local  4096  8711 KB    5220 KB    UP     [UP]
+    # 2   Internet. . . . . . . . . . . .  wan    1492  555 MB     606 MB     UP     UP
+    # 3   LocalNetwork. . . . . . . . . .  lan    1456  647 MB     561 MB     UP     [UP]
+
+    # We're looking for line #2 in the above example
+    regex = re.compile('\d   Internet(?:\. )* wan \s*\d*\s* (\d*) (KB|MB|GB|TB) \s* (\d*) (KB|MB|GB|TB) ')
+    for row in if_stats.split('\n'):
+        matches = regex.findall(row)
+
+        if len(matches) > 0:
+            matches = matches[0]
+            rx = int(matches[0])
+            rx_unit = matches[1]
+            tx = int(matches[2])
+            tx_unit = matches[3]
+
+            # Handle units
+            units = {
+                'KB': 1000,
+                'MB': 1000000,
+                'GB': 1000000000,
+                'TB': 1000000000000
+            }
+
+            rx *= units[rx_unit]
+            tx *= units[tx_unit]
+
+            # Subtract last saved value
+            last_rx = rx
+            last_tx = tx
+
+            # May not exist on first run
+            if os.path.isfile(IF_LOG_FILE_PATH):
+                with open(IF_LOG_FILE_PATH) as f:
+                    last_rx, last_tx = pickle.load(f)
+
+                last_rx = int(last_rx)
+                last_tx = int(last_tx)
+
+            # Save current value
+            with open(IF_LOG_FILE_PATH, 'w') as f:
+                pickle.dump([rx, tx], f)
+
+            # Will be 0 on first run, perfect
+            five_minutes_rx = rx - last_rx
+            five_minutes_tx = tx - last_tx
+
+            one_sec_rx = five_minutes_rx / (5 * 60)
+            one_sec_tx = five_minutes_tx / (5 * 60)
+
+            print('up.value ' + str(one_sec_tx))
+            print('down.value ' + str(one_sec_rx))
+
+            sys.exit(0)
+
+    # Couldn't find regex match
+    sys.exit(1)
